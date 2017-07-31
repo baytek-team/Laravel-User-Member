df64b59eb85a562aa2c511d39c6c4c0fac6b8cff
Sarah Marinoff <sarah@baytek.ca>
2017-07-31 11:51:59 -0400

    updated admin route urls

--- src/Routes/admin/member.php
+++ src/Routes/admin/member.php
@@ -1,11 +1,11 @@
 <?php
 
-Route::get('members/pending', 'AdminController@pending')->name('members.pending');
-Route::get('members/deleted', 'AdminController@deleted')->name('members.deleted');
-Route::get('members/administrators', 'AdminController@administrators')->name('members.adminindex');
+Route::get('pending', 'AdminController@pending')->name('members.pending');
+Route::get('deleted', 'AdminController@deleted')->name('members.deleted');
+Route::get('administrators', 'AdminController@administrators')->name('members.adminindex');
 
-Route::post('members/{user}/approve', 'AdminController@approve')->name('members.approve');
-Route::post('members/{user}/decline', 'AdminController@decline')->name('members.decline');
-Route::post('members/{user}/password/email', 'AdminController@sendPasswordResetLink')->name('member.password.email');
+Route::post('{user}/approve', 'AdminController@approve')->name('members.approve');
+Route::post('{user}/decline', 'AdminController@decline')->name('members.decline');
+Route::post('{user}/password/email', 'AdminController@sendPasswordResetLink')->name('member.password.email');
 
-Route::resource('members', AdminController::class);
+Route::resource('/', AdminController::class);
