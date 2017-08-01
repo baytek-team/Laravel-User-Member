df1295f5393617863922d4a660d28c500ae7c505
Sarah Marinoff <sarah@baytek.ca>
2017-08-01 17:08:57 -0400

    seed demo member and admin

--- src/Seeders/FakeDataSeeder.php
+++ src/Seeders/FakeDataSeeder.php
@@ -16,14 +16,88 @@ class FakeDataSeeder extends Seeder
      */
     public function run()
     {
+        //First create a generic admin and member, that visitors to
+        //a demo site can sign in with and test as
+        $this->generateTestAccounts();
+        
+        //Then seed the fake members
         $this->generateUsers();
+    }
+
+    public function generateTestAccounts()
+    {
+        /**
+         * The fake admin
+         */
+        $meta = [
+            'first_name' => 'Demo',
+            'last_name' => 'Admin',
+            'work_phone' => '555-555-5555',
+            'home_phone' => '123-345-7890',
+            'company' => 'Baytek',
+            'title' => 'Tester',
+        ];
+
+        //Save the user
+        $admin = new Member();
+        $admin->email = 'baytek_admin@example.com';
+        $admin->name = $first_name . ' ' . $last_name;
+        $admin->password = bcrypt('password');
+        $admin->save();
+
+        //Save the user meta
+        foreach($meta as $key => $value) {
+            $metaRecord = (new UserMeta([
+                'language' => 'en',
+                'key' => $key,
+                'value' => $value
+            ]));
+            $admin->meta()->save($metaRecord);
+            $metaRecord->save();
+        }
+
+        $admin->assignRole(['Administrator']);
+        $admin->onBit(Member::APPROVED)->update();
+
+        /**
+         * The fake member
+         */
+        $meta = [
+            'first_name' => 'Demo',
+            'last_name' => 'Member',
+            'work_phone' => '555-555-5555',
+            'home_phone' => '123-345-7890',
+            'company' => 'Baytek',
+            'title' => 'Tester',
+        ];
+
+        //Save the user
+        $member = new Member();
+        $member->email = 'baytek_member@example.com';
+        $member->name = $first_name . ' ' . $last_name;
+        $member->password = bcrypt('password');
+        $member->save();
+
+        //Save the user meta
+        foreach($meta as $key => $value) {
+            $metaRecord = (new UserMeta([
+                'language' => 'en',
+                'key' => $key,
+                'value' => $value
+            ]));
+            $member->meta()->save($metaRecord);
+            $metaRecord->save();
+        }
+
+        $member->assignRole(['Member']);
+        $member->onBit(Member::APPROVED)->update();
     }
 
     public function generateUsers($total = 50)
     {
         $faker = Faker::create();
 
-        foreach(range(1,$total) as $index) {
+        foreach (range(1,$total) as $index) {
 
             //Member info
             $first_name = $faker->firstName;
