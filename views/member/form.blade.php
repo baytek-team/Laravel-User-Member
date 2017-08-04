ec65991440588ff7161a7ff48450ce7fc09891f1
Yvon Viger <yvon@baytek.ca>
2017-08-04 14:13:05 -0400

    Adding floating label

--- views/member/form.blade.php
+++ views/member/form.blade.php
@@ -15,33 +15,33 @@
 @endsection
 
 <div class="two fields">
-    <div class="field{{ $errors->has('meta.first_name') ? ' error' : '' }}">
+    <div class="floaty field{{ $errors->has('meta.first_name') ? ' error' : '' }}">
         <label for="first_name">{{ ___('First Name') }}</label>
         <input type="text" id="first_name" name="meta[first_name]" placeholder="{{ ___('First Name') }}" value="{{ old('meta.first_name', $user->metadata('first_name')) }}">
     </div>
-    <div class="field{{ $errors->has('meta.last_name') ? ' error' : '' }}">
+    <div class="floaty field{{ $errors->has('meta.last_name') ? ' error' : '' }}">
         <label for="last_name">{{ ___('Last Name') }}</label>
         <input type="text" id="last_name" name="meta[last_name]" placeholder="{{ ___('Last Name') }}" value="{{ old('meta.last_name', $user->metadata('last_name')) }}">
     </div>
 </div>
 
-<div class="two fields">
-    <div class="field{{ $errors->has('email') ? ' error' : '' }}">
+<div class="two floaty fields">
+    <div class="floaty field{{ $errors->has('email') ? ' error' : '' }}">
         <label for="email">{{ ___('Email Address') }}</label>
         <input type="text" id="email" name="email" @if($user->id) disabled @endif placeholder="{{ ___('Email Address') }}" value="{{ old('email', $user->email) }}">
     </div>
-    <div class="field{{ $errors->has('meta.title') ? ' error' : '' }}">
+    <div class="floaty field{{ $errors->has('meta.title') ? ' error' : '' }}">
         <label for="title">{{ ___('Title') }}</label>
         <input type="text" id="title" name="meta[title]" placeholder="{{ ___('Title') }}" value="{{ old('meta.title', $user->metadata('title')) }}">
     </div>
 </div>
 
-<div class="two fields">
-    <div class="field{{ $errors->has('meta.home_phone') ? ' error' : '' }}">
+<div class="two floaty fields">
+    <div class="floaty field{{ $errors->has('meta.home_phone') ? ' error' : '' }}">
         <label for="home_phone">{{ ___('Home Phone') }}</label>
         <input type="text" id="home_phone" name="meta[home_phone]" placeholder="{{ ___('Phone') }}" value="{{ old('meta.home_phone', $user->metadata('home_phone')) }}">
     </div>
-    <div class="field{{ $errors->has('meta.work_phone') ? ' error' : '' }}">
+    <div class="floaty field{{ $errors->has('meta.work_phone') ? ' error' : '' }}">
         <label for="work_phone">{{ ___('Work Phone') }}</label>
         <input type="text" id="work_phone" name="meta[work_phone]" placeholder="{{ ___('Phone') }}" value="{{ old('meta.work_phone', $user->metadata('work_phone')) }}">
     </div>
