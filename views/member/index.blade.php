982d7d6568df2f9cad66f7d2e7d6ef3808134875
Yvon Viger <yvon@baytek.ca>
2017-08-17 09:50:56 -0400

    Addressing the issue of no results

--- views/member/index.blade.php
+++ views/member/index.blade.php
@@ -27,72 +27,86 @@
     </div>
 @endsection
 
-@section('content')
-<table class="ui selectable very basic table">
-    <thead>
-        <tr>
-            <th>{{ ___('Name') }}</th>
-            <th>{{ ___('Email') }}</th>
-            <th class="center aligned collapsing">{{ ___('Actions') }}</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse($members as $member)
-            <tr data-member-id="{{ $member->id }}">
-                <td>{{ $member->name }}</td>
-                <td>{{ $member->email }}</td>
-                <td class="collapsing right aligned">
-                    <div class="ui text compact menu">
-                        @can('update', $member)
-                        <a href="{{ route('members.edit', ['member' => $member]) }}" class="item">
-                            <i class="pencil icon"></i> {{-- {{ ___('Edit') }} --}}
-                        </a>
-                        @endcan
-
-                        @if (Auth::user()->hasRole([\Baytek\Laravel\Users\Roles\Root::ROLE]))
-                            <a class="item" href="{{ route('user.roles', ['user' => $member]) }}">
-                                <i class="user icon"></i>{{--  {{ ___('Roles') }} --}}
-                            </a>
-                        @endif
-
-                        @can('update', $member)
-                            @if ($filter != 'active')
-                                @button(___('Approve'), [
-                                    'method' => 'post',
-                                    'location' => 'members.approve',
-                                    'type' => 'route',
-                                    'confirm' => '<h1 class=\'ui inverted header\'>'.___('Approve this member?').'<div class=\'sub header\'>'.$member->name.'</div></h1>',
-                                    'class' => 'item action',
-                                    'prepend' => '<i class="checkmark icon"></i>',
-                                    'model' => $member,
-                                ])
-                            @endif
-
-                            @if ($filter != 'deleted')
-                                @button('', [
-                                    'method' => 'post',
-                                    'location' => 'members.decline',
-                                    'type' => 'route',
-                                    'confirm' => '<h1 class=\'ui inverted header\'>'.___('Delete this member?').'<div class=\'sub header\'>'.$member->name.'</div></h1>',
-                                    'class' => 'item action',
-                                    'prepend' => '<i class="delete icon"></i>',
-                                    'model' => $member,
-                                ])
-                            @endif
-                        @endcan
-                    </div>
-                </td>
-            </tr>
-        @empty
+@if(count($members))
+    @section('content')
+    <table class="ui selectable very basic table">
+        <thead>
             <tr>
-                <td colspan="3">
-                    <div class="ui centered">{{ ___('There are no results') }}</div>
-                </td>
+                <th>{{ ___('Name') }}</th>
+                <th>{{ ___('Email') }}</th>
+                <th class="center aligned collapsing">{{ ___('Actions') }}</th>
             </tr>
-        @endforelse
-    </tbody>
-</table>
+        </thead>
+        <tbody>
+            @forelse($members as $member)
+                <tr data-member-id="{{ $member->id }}">
+                    <td>{{ $member->name }}</td>
+                    <td>{{ $member->email }}</td>
+                    <td class="collapsing right aligned">
+                        <div class="ui text compact menu">
+                            @can('update', $member)
+                            <a href="{{ route('members.edit', ['member' => $member]) }}" class="item">
+                                <i class="pencil icon"></i> {{-- {{ ___('Edit') }} --}}
+                            </a>
+                            @endcan
 
-{{ $members->appends(collect(Request::instance()->query)->toArray())->links('pagination.default') }}
+                            @if (Auth::user()->hasRole([\Baytek\Laravel\Users\Roles\Root::ROLE]))
+                                <a class="item" href="{{ route('user.roles', ['user' => $member]) }}">
+                                    <i class="user icon"></i>{{--  {{ ___('Roles') }} --}}
+                                </a>
+                            @endif
 
-@endsection
+                            @can('update', $member)
+                                @if ($filter != 'active')
+                                    @button(___('Approve'), [
+                                        'method' => 'post',
+                                        'location' => 'members.approve',
+                                        'type' => 'route',
+                                        'confirm' => '<h1 class=\'ui inverted header\'>'.___('Approve this member?').'<div class=\'sub header\'>'.$member->name.'</div></h1>',
+                                        'class' => 'item action',
+                                        'prepend' => '<i class="checkmark icon"></i>',
+                                        'model' => $member,
+                                    ])
+                                @endif
+
+                                @if ($filter != 'deleted')
+                                    @button('', [
+                                        'method' => 'post',
+                                        'location' => 'members.decline',
+                                        'type' => 'route',
+                                        'confirm' => '<h1 class=\'ui inverted header\'>'.___('Delete this member?').'<div class=\'sub header\'>'.$member->name.'</div></h1>',
+                                        'class' => 'item action',
+                                        'prepend' => '<i class="delete icon"></i>',
+                                        'model' => $member,
+                                    ])
+                                @endif
+                            @endcan
+                        </div>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td colspan="3">
+                        <div class="ui centered">{{ ___('There are no results') }}</div>
+                    </td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
+
+    {{ $members->appends(collect(Request::instance()->query)->toArray())->links('pagination.default') }}
+
+    @endsection
+@else
+    @section('outer-content')
+        <div class="ui middle aligned padded grid no-result">
+            <div class="column">
+                <div class="ui center aligned padded grid">
+                    <div class="column">
+                        <h2>{{ ___('We couldn\'t find anything') }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endsection
+@endif
